43542e2bbca58c2b8a256decf6f8465b757160b4
Roger Gabriel <munizferreirar@gmail.com>
2026-04-25 16:11:41 -0300

    Adicionando o projeto principal

---
+++ app/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// Importa o componente visual da pasta screens
+// Atenção: ajuste os '../' conforme a real distância entre a pasta app e a pasta screens no seu projeto
+import HomeScreenUI from '../src/screens/HomeScreen';
+
+export default function HomeScreenRoute() {
+  return <HomeScreenUI />;
+}
